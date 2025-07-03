3c8292c471186e0290e28771ec397215ee377e31
Halffd <guhalfeld@hotmail.com>
2025-07-02 23:45:32 -0300

    fix compiler header

--- src/havel-lang/compiler/Compiler.h
+++ src/havel-lang/compiler/Compiler.h
@@ -2,24 +2,28 @@
 
 #include "../ast/AST.h"
 
-#include "../llvm/LLVMWrapper.h"
+// Guard against macro conflicts
+#ifdef emit
+#undef emit
+#endif
 
-using llvm::LLVMContext;
-using llvm::Module;
-using llvm::Function;
-using llvm::BasicBlock;
-using llvm::Value;
-using llvm::Type;
-using llvm::IRBuilder;
-using llvm::StringRef;
+// Include LLVM headers in correct order
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Value.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/Verifier.h>
 
-// Forward declare LLVM types we need
-namespace llvm {
-class ExecutionEngine;
-class TargetMachine;
-class DataLayout;
-} // namespace llvm
-
+// Standard library includes
 #include <memory>
 #include <unordered_map>
 #include <string>
@@ -31,12 +35,12 @@ class Compiler {
 private:
     llvm::LLVMContext context;
     llvm::IRBuilder<> builder;
-    llvm::Module* module;
+    std::unique_ptr<llvm::Module> module;
     std::unique_ptr<llvm::ExecutionEngine> executionEngine;
 
-    // Symbol tables - YOUR VARIABLES AND FUNCTIONS! 🔥
-    std::unordered_map<std::string, llvm::Value*> namedValues;     // Variables
-    std::unordered_map<std::string, llvm::Function*> functions;    // Functions
+    // Symbol tables
+    std::unordered_map<std::string, llvm::Value*> namedValues;
+    std::unordered_map<std::string, llvm::Function*> functions;
 
 public:
     Compiler();
@@ -53,7 +57,7 @@ public:
     typedef void (*HotkeyActionFunc)();
     HotkeyActionFunc GetCompiledFunction(const std::string& name);
 
-    // AST to LLVM IR generation - ADAPTED TO YOUR AST! 💪
+    // AST to LLVM IR generation
     llvm::Value* GenerateExpression(const ast::Expression& expr);
     llvm::Value* GenerateStatement(const ast::Statement& stmt);
 
@@ -64,7 +68,7 @@ public:
     llvm::Value* GenerateMember(const ast::MemberExpression& member);
     llvm::Value* GenerateBinary(const ast::BinaryExpression& binary);
 
-    // Literals - PERFECTLY ADAPTED TO YOUR AST! ⚡
+    // Literals
     llvm::Value* GenerateStringLiteral(const ast::StringLiteral& str);
     llvm::Value* GenerateNumberLiteral(const ast::NumberLiteral& num);
     llvm::Value* GenerateIdentifier(const ast::Identifier& id);

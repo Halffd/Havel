a0702eb61da677ad0a5a123c790c622859d9884f
Halffd <guhalfeld@hotmail.com>
2025-12-09 07:16:31 -0300

    Fix AutoRunner

--- src/core/automation/AutoRunner.cpp
+++ src/core/automation/AutoRunner.cpp
@@ -1,63 +1,104 @@
 #include "AutoRunner.hpp"
+#include "../utils/Timer.hpp"
+#include "../window/WindowManager.hpp"
 #include <stdexcept>
 #include <utility>
 #include "utils/Logger.hpp"
 
-namespace havel::automation {
+namespace havel::automation
+{
 
-AutoRunner::AutoRunner(std::shared_ptr<IO> io)
-    : AutoPresser("AutoRunner", std::chrono::milliseconds(100))
-    , io_(std::move(io))
-    , direction_("w") {
-    if (!io_) {
-        throw std::invalid_argument("IO cannot be null");
+    AutoRunner::AutoRunner(std::shared_ptr<IO> io)
+        : AutoPresser("AutoRunner", std::chrono::milliseconds(100)), io_(std::move(io)), direction_("w")
+    {
+        if (!io_)
+        {
+            throw std::invalid_argument("IO cannot be null");
+        }
+        // Don't setup actions - we'll handle everything in onStart/onStop
     }
-    // Don't setup actions - we'll handle everything in onStart/onStop
-}
 
-void AutoRunner::setDirection(const std::string& direction) {
-    if (direction.empty()) {
-        throw std::invalid_argument("Direction cannot be empty");
+    void AutoRunner::setDirection(const std::string &direction)
+    {
+        if (direction.empty())
+        {
+            throw std::invalid_argument("Direction cannot be empty");
+        }
+
+        bool wasRunning = isRunning();
+        if (wasRunning)
+        {
+            stop();
+        }
+
+        direction_ = direction;
+
+        if (wasRunning)
+        {
+            start();
+        }
     }
-    
-    bool wasRunning = isRunning();
-    if (wasRunning) {
-        stop();
+
+    void AutoRunner::setIntervalMs(int intervalMs)
+    {
+        setInterval(std::chrono::milliseconds(intervalMs));
     }
-    
-    direction_ = direction;
-    
-    if (wasRunning) {
-        start();
+    void AutoRunner::onStart()
+    {
+        // JUST hold the keys down - no spamming needed!
+        io_->Send("{" + direction_ + ":down}");
+
+        auto winId = WindowManager::GetActiveWindow();
+        timer = TimerManager::SetTimer(1700, [this, winId]()
+                                       {
+          if (WindowManager::GetActiveWindow() != winId) {
+              havel::info("Window changed, stopping F timer");
+              if (timer) {
+                  TimerManager::StopTimer(timer);
+                  timer = nullptr;
+              }
+              io_->Send("{" + direction_ + ":up}");
+              io_->Send("{LShift:up}");
+              return;
+          }
+
+          if (io_->GetKeyState(direction_)) {
+              // Stop mode
+              if (timer) {
+                  TimerManager::StopTimer(timer);
+                  timer = nullptr;
+              }
+              io_->Send("{" + direction_ + ":up}");
+              io_->Send("{LShift:up}");
+              return;  // 👈 EARLY EXIT
+          }
+          io_->Send("{LShift}");
+        }, true);  // repeating=true
+        havel::info("AutoRunner started - holding {} and Shift", direction_);
     }
-}
 
-void AutoRunner::setIntervalMs(int intervalMs) {
-    setInterval(std::chrono::milliseconds(intervalMs));
-}
+    void AutoRunner::onStop()
+    {
+        // Release both keys
+        io_->Send("{" + direction_ + ":up}");
+        io_->Send("{LShift:up}");
+        if (timer)
+        {
+            TimerManager::StopTimer(timer);
+            timer = nullptr;
+        }
+        havel::info("AutoRunner stopped - released {} and Shift", direction_);
+    }
 
-void AutoRunner::onStart() {
-    // JUST hold the keys down - no spamming needed!
-    io_->Send("{" + direction_ + ":down}");
-    io_->Send("{LShift:down}");
-    info("AutoRunner started - holding {} and Shift", direction_);
-}
+    void AutoRunner::setupDirectionAction()
+    {
+        // DON'T SET PRESS/RELEASE ACTIONS!
+        // AutoPresser will call onPress/onRelease which we don't want for holding keys
+        // We only want onStart/onStop for hold-style automation
 
-void AutoRunner::onStop() {
-    // Release both keys
-    io_->Send("{" + direction_ + ":up}");
-    io_->Send("{LShift:up}");
-    info("AutoRunner stopped - released {} and Shift", direction_);
-}
-
-void AutoRunner::setupDirectionAction() {
-    // DON'T SET PRESS/RELEASE ACTIONS!
-    // AutoPresser will call onPress/onRelease which we don't want for holding keys
-    // We only want onStart/onStop for hold-style automation
-    
-    // Clear any actions to prevent AutoPresser from doing press/release cycles
-    setPressAction(nullptr);
-    setReleaseAction(nullptr);
-}
+        // Clear any actions to prevent AutoPresser from doing press/release cycles
+        setPressAction(nullptr);
+        setReleaseAction(nullptr);
+    }
 
 } // namespace havel::automation

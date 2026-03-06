2e1b99c7c02a0edd17a6eb2ef8d1ea858e6e6cd7
half-arch <half-arch@users.noreply.github.com>
2026-03-06 05:53:03 -0300

    refactor: extract Environment class to separate file
    Moved Environment and TraitRegistry classes to:
    - src/havel-lang/runtime/Environment.hpp (declaration)
    - src/havel-lang/runtime/Environment.cpp (implementation)
    
    Benefits:
    - Better code organization
    - Reduced Interpreter.hpp size (90 lines removed)
    - Reduced Interpreter.cpp size (45 lines removed)
    - Clearer separation of concerns
    
    Note: Environment.hpp includes Interpreter.hpp for HavelValue.
    This is acceptable for now - future refactoring can move
    HavelValue to a separate Value.hpp if needed.

--- src/havel-lang/runtime/Interpreter.cpp
+++ src/havel-lang/runtime/Interpreter.cpp
@@ -19,6 +19,7 @@
 #include "process/Launcher.hpp"
 #include "qt.hpp"
 #include "window/WindowManagerDetector.hpp"
+#include "Environment.hpp"  // For Environment and TraitRegistry
 #include <QBuffer>
 #include <QClipboard>
 #include <QGuiApplication>
@@ -2943,51 +2944,6 @@ void Interpreter::visitImplDeclaration(const ast::ImplDeclaration &node) {
   lastResult = nullptr;
 }
 
-// TraitRegistry implementation
-void TraitRegistry::registerImpl(const std::string& traitName, const std::string& typeName,
-                                  std::unordered_map<std::string, HavelValue> methods) {
-  TraitImpl impl;
-  impl.traitName = traitName;
-  impl.typeName = typeName;
-  impl.methods = std::move(methods);
-  
-  typeImpls[typeName].push_back(impl);
-  implMap[typeName][traitName] = impl;
-}
-
-bool TraitRegistry::implements(const std::string& typeName, const std::string& traitName) const {
-  auto typeIt = implMap.find(typeName);
-  if (typeIt != implMap.end()) {
-    return typeIt->second.find(traitName) != typeIt->second.end();
-  }
-  return false;
-}
-
-std::vector<const TraitImpl*> TraitRegistry::getImplsForType(const std::string& typeName) const {
-  std::vector<const TraitImpl*> result;
-  auto typeIt = typeImpls.find(typeName);
-  if (typeIt != typeImpls.end()) {
-    for (const auto& impl : typeIt->second) {
-      result.push_back(&impl);
-    }
-  }
-  return result;
-}
-
-HavelValue TraitRegistry::getMethod(const std::string& typeName, const std::string& traitName,
-                                     const std::string& methodName) const {
-  auto typeIt = implMap.find(typeName);
-  if (typeIt != implMap.end()) {
-    auto traitIt = typeIt->second.find(traitName);
-    if (traitIt != typeIt->second.end()) {
-      auto methodIt = traitIt->second.methods.find(methodName);
-      if (methodIt != traitIt->second.methods.end()) {
-        return methodIt->second;
-      }
-    }
-  }
-  return HavelValue(nullptr);
-}
 
 void Interpreter::visitForStatement(const ast::ForStatement &node) {
   // Evaluate the iterable expression

a4cbabc83a63ea7f60657aaa1f7303be4ab2d523
Half <guhalfeld@hotmail.com>
2026-03-11 07:24:16 -0300

    fix: use weak_ptr for hotkey interpreter reference to prevent dangling pointers

--- src/modules/hotkey/HotkeyModule.hpp
+++ src/modules/hotkey/HotkeyModule.hpp
@@ -6,6 +6,7 @@
 #pragma once
 
 #include "../../host/HostContext.hpp"
+#include <memory>
 
 namespace havel {
 
@@ -16,8 +17,8 @@ namespace modules {
 
 void registerHotkeyModule(Environment& env, HostContext& ctx);
 
-// Set the global interpreter reference for hotkey callbacks
-void SetHotkeyInterpreter(Interpreter* interp);
+// Set the global interpreter reference for hotkey callbacks (uses weak_ptr for safety)
+void SetHotkeyInterpreter(std::weak_ptr<Interpreter> interp);
 
 } // namespace modules
 } // namespace havel

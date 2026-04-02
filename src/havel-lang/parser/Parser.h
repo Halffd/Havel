165df0ed54913f0ef52822f24775a8b22693bcf5
Half <guhalfeld@hotmail.com>
2026-04-02 18:02:26 -0300

    fix: add using directives and fix parseExpressionFromString scope

--- src/havel-lang/parser/Parser.h
+++ src/havel-lang/parser/Parser.h
@@ -274,6 +274,7 @@ private:
   std::unique_ptr<ast::Expression> parseParenthesizedExpression();
   std::unique_ptr<ast::Expression> parseBacktickExpression();
   std::unique_ptr<ast::Expression> parseLambdaExpression();
+  std::unique_ptr<ast::Expression> parseExpressionFromString(const std::string &expr);
 
 public:
   explicit Parser(const DebugOptions &debug_opts = {}) : debug(debug_opts) {}

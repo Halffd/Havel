a5821362fc23403ee7e39fdc72373c7ae7690541
Half <guhalfeld@hotmail.com>
2026-04-17 16:15:29 -0300

    cleanup: remove deprecated Scheduler_v2 files

--- src/havel-lang/runtime/concurrency/Scheduler_v2.cpp
+++
@@ -1,7 +0,0 @@
-// DEPRECATED - Use Scheduler.cpp instead
-// This file is empty (consolidated into Scheduler.cpp)
-// The build system will pick up Scheduler.cpp via GLOB_RECURSE
-
-// This file is kept for compatibility but has no implementation.
-// All functionality is now in Scheduler.cpp / Scheduler.hpp
-

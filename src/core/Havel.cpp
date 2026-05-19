15cfe21a2dbb481def073a4b56bf9581767c8b11
Half <guhalfeld@hotmail.com>
2026-05-19 20:32:58 -0300

    fix: correct Debug.AutoExit cancel logic and use graceful exit

--- src/core/Havel.cpp
+++ src/core/Havel.cpp
@@ -316,18 +316,17 @@ if (hotkeyManager) {
       throw std::runtime_error("Failed to open X11 display");
     }
   }
-  if(Configs::Get().Get<bool>("Debug.AutoExit", false)){
-    std::thread([this]() {
-      auto s = Configs::Get().Get<int>("Debug.AutoExitDelay", 15);
-      std::this_thread::sleep_for(std::chrono::seconds(s));
-      if(Configs::Get().Get<bool>("Debug.AutoExit", false)){
-        return; // AutoExit was disabled during the wait
-      }
-      if (debugging::debug_io) debug("AutoExit enabled - exiting after {} seconds", s);
-      std::exit(0);
-      this->exit();
-    }).detach();
-  }
+if(Configs::Get().Get<bool>("Debug.AutoExit", false)){
+std::thread([this]() {
+auto s = Configs::Get().Get<int>("Debug.AutoExitDelay", 15);
+std::this_thread::sleep_for(std::chrono::seconds(s));
+if(!Configs::Get().Get<bool>("Debug.AutoExit", false)){
+return; // AutoExit was disabled during the wait
+}
+if (debugging::debug_io) debug("AutoExit enabled - exiting after {} seconds", s);
+this->exit();
+}).detach();
+}
 }
 
 void Havel::cleanup() noexcept {

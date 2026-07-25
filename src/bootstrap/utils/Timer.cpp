a0a5aaaee8808fad7fd60ba92de2be1737938f8c
Half <guhalfeld@hotmail.com>
2026-07-25 10:46:52 -0300

    remove dupliucates

--- src/bootstrap/utils/Timer.cpp
+++
@@ -1,7 +0,0 @@
-#include "Timer.hpp"
-namespace havel {
-std::unordered_map<std::shared_ptr<std::atomic<bool>>,
-                   std::shared_ptr<TimerManager::TimerInfo>>
-    TimerManager::activeTimers;
-std::shared_mutex TimerManager::timersMutex;
-} // namespace havel
